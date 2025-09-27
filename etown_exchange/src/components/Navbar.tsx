1513542e0e7ef22b14b6a61112d799916683aa91
Mildred <nwachukwu.mildred10@gmail.com>
2025-09-26 21:08:02 -0400

    created necessary folders and installed dependencies

---
+++ etown_exchange/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function Navbar() {
+    return(
+        <nav>
+            {/* Etown logo? */}
+
+            {/* Nav links */}
+            <div>
+                <Link to="./"> Placeholder</Link>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ etown_exchange/src/screens/LoginScreen.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function LoginScreen(){
+
+}
+
+export default LoginScreen;
