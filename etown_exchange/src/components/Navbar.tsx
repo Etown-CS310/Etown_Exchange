ce9aa165b686493bc438d84442daf31afbc50d44
Mildred <nwachukwu.mildred10@gmail.com>
2025-11-29 02:14:58 -0500

    added link to profile on navbar

--- etown_exchange/src/components/Navbar.tsx
+++ etown_exchange/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Link, useNavigate, useLocation} from 'react-router-dom';
-import {useAuth} from '../auth/authContext';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useAuth } from '../auth/authContext';
 import Logo from './Logo';
 import Button from '../components/Button';
 import './styles/Navbar.css';
@@ -9,43 +9,46 @@ import './styles/Navbar.css';
 const Navbar: React.FC = () => {
     const navigate = useNavigate();
     const location = useLocation();
-    const {currentUser, signOut} = useAuth();
+    const { currentUser, signOut } = useAuth();
 
-    const handleSignOut = async() => {
-        try{
+    const handleSignOut = async () => {
+        try {
             signOut();
             navigate('/');
-        } catch (err: any){
+        } catch (err: any) {
             console.log(err)
         }
     }
 
-    return(
+    return (
         <nav className="navbar">
             {/* logo */}
             <div className="navbar-left">
-                <Logo 
-                size="medium"
-                onClick={() => navigate('/dashboard')}
+                <Logo
+                    size="medium"
+                    onClick={() => navigate('/dashboard')}
                 />
             </div>
 
             {/* nav links */}
             <div className="navbar-middle">
-                <Link to='/dashboard' 
-                className={location.pathname === '/dashboard' ? 'nav-link active' : 'nav-link'}
+                <Link to='/dashboard'
+                    className={location.pathname === '/dashboard' ? 'nav-link active' : 'nav-link'}
                 >
-                Browse
+                    Browse
                 </Link>
-                <Link to="/create-listing" 
-                className={location.pathname === '/create-listing' ? 'nav-link active' : 'nav-link'}
+                <Link to="/create-listing"
+                    className={location.pathname === '/create-listing' ? 'nav-link active' : 'nav-link'}
                 >
-                Post Listing
+                    Post Listing
                 </Link>
-                <Link to="/my-listings" 
-                className={location.pathname === '/my-listings' ? 'nav-link active' : 'nav-link'}
+                <Link to="/my-listings"
+                    className={location.pathname === '/my-listings' ? 'nav-link active' : 'nav-link'}
                 >
-                My Listings
+                    My Listings
+                </Link>
+                <Link to="/profile" className={location.pathname === '/profile' ? 'nav-link active' : 'nav-link'}>
+                    Profile
                 </Link>
             </div>
 
@@ -54,10 +57,10 @@ const Navbar: React.FC = () => {
                 <span className="user-email">
                     {currentUser?.email}
                 </span>
-                <Button 
-                text="Sign Out"
-                onClick={handleSignOut}
-                className="sign-out-btn"/>
+                <Button
+                    text="Sign Out"
+                    onClick={handleSignOut}
+                    className="sign-out-btn" />
 
             </div>
         </nav>
